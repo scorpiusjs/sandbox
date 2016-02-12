85af30c66e866884a7f63c16fc9bc152b5f63cc4
Ryan Watts <ryandwatts@gmail.com>
2016-02-12 10:24:34 -0700

    scorpiusjs:sandbox Initial Commit

---
+++ scorpiusjs-sandbox.js
@@ -0,0 +1,29 @@
+
+import scorpius from 'meteor/scorpiusjs:core';
+
+if (Meteor.isClient) {
+  
+  console.log(scorpius.config);
+  
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+	console.log(scorpius);
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
