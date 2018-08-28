dee8d5fa6e8d6bb94843bb5bb25b714f189727a7
Ryan Watts <rwatts@220OFFICE.local>
2018-08-28 15:26:02 -0700

    updates

--- scorpiusjs-sandbox.js
+++ scorpiusjs-sandbox.js
@@ -2,28 +2,28 @@
 import scorpius from 'meteor/scorpiusjs:core';
 
 if (Meteor.isClient) {
-  
-  console.log(scorpius.config);
-  
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+	console.log(scorpius.config);
+
+	// counter starts at 0
+	Session.setDefault('counter', 0);
+	Template.hello.helpers({
+		counter: function () {
+			return Session.get('counter');
+		}
+	});
+
+	Template.hello.events({
+		'click button': function () {
+			// increment the counter when button is clicked
+			Session.set('counter', Session.get('counter') + 1);
+		}
+	});
 }
 
 if (Meteor.isServer) {
 	console.log(scorpius);
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+	Meteor.startup(function () {
+		// code to run on server at startup
+	});
 }
